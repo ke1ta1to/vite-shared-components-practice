c71cb5d290069920175565708983dbe24a837232
Keita Ito <keitaito@keitaito.net>
2025-06-24 02:29:26 +0900

    fix: source references

--- apps/user/vite.config.ts
+++ apps/user/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@vite-shared-components-practice/shared-components': resolve(__dirname, '../../packages/shared-components/src')
+    }
+  }
 })
